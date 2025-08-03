d75c5a21ae85f56abc5cd7a395fafa0f5cd8d99c
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2025-08-03 16:58:27 -0300

    Wip: Setup

---
+++ src/service/download.service.test.js
@@ -0,0 +1,11 @@
+import { expect, describe, test, jest, beforeEach } from "@jest/globals"
+import { Server } from "http"
+
+const originalHttp = jest.createMockFromModule("http")
+
+describe("Http Download Test", () => {
+
+    test.todo("should receive url from input and check if it is valid")
+    test.todo("should download a file from a URL")
+
+})
