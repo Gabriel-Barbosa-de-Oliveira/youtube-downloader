a264a5b6d93eba168e4ebf286a900bfddcf6abd8
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2025-08-03 17:23:56 -0300

    Wip: Setup

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import { createServer } from 'http'
 import { statusCodes } from './utils/httpStatusCode.js'
+import DownloadService from './service/download.service.js'
+
+
+
 
 async function handler(request, response) {
-    let body = ''
-    request.on('data', chunk => {
-        body += chunk
-    })
-    
-    request.on('end', () => {
-        response.writeHead(statusCodes.OK, { 'Content-Type': 'text/plain' })
-        response.end(body)
-    })
+    const downloadService = new DownloadService()
+
+    // Example usage of the download service
+    await downloadService.downloadFile();
+
+    response.writeHead(statusCodes.OK)
+    response.end()
 }
 
 createServer(handler).listen(3000, () => console.log('running at 3000'))
--- src/service/download.service.js
+++ src/service/download.service.js
@@ -1,17 +1,37 @@
-const ytdl = require("@distube/ytdl-core");
-// TypeScript: import ytdl from '@distube/ytdl-core'; with --esModuleInterop
-// TypeScript: import * as ytdl from '@distube/ytdl-core'; with --allowSyntheticDefaultImports
-// TypeScript: import ytdl = require('@distube/ytdl-core'); with neither of the above
+import * as ytdl from '@distube/ytdl-core';
 
-// Download a video
-ytdl("http://www.youtube.com/watch?v=aqz-KE-bpKQ").pipe(require("fs").createWriteStream("video.mp4"));
+// Polyfill para File no Node.js (corrige erro do undici)
+if (typeof File === 'undefined') {
+  global.File = class File extends Blob {
+    constructor(chunks, filename, options = {}) {
+      super(chunks, options);
+      this.name = filename;
+      this.lastModified = options.lastModified || Date.now();
+    }
+  };
+}
 
-// Get video info
-ytdl.getBasicInfo("http://www.youtube.com/watch?v=aqz-KE-bpKQ").then(info => {
-  console.log(info.videoDetails.title);
-});
+export default class DownloadService {
 
-// Get video info with download formats
-ytdl.getInfo("http://www.youtube.com/watch?v=aqz-KE-bpKQ").then(info => {
-  console.log(info.formats);
-});
+    constructor() { }
+
+    async downloadFile() {
+        // TypeScript: import ytdl from '@distube/ytdl-core'; with --esModuleInterop
+        // TypeScript: import * as ytdl from '@distube/ytdl-core'; with --allowSyntheticDefaultImports
+        // TypeScript: import ytdl = require('@distube/ytdl-core'); with neither of the above
+
+        // Download a video
+        ytdl("http://www.youtube.com/watch?v=aqz-KE-bpKQ").pipe(require("fs").createWriteStream("video.mp4"));
+
+        // Get video info
+        ytdl.getBasicInfo("http://www.youtube.com/watch?v=aqz-KE-bpKQ").then(info => {
+            console.log(info.videoDetails.title);
+        });
+
+        // Get video info with download formats
+        ytdl.getInfo("http://www.youtube.com/watch?v=aqz-KE-bpKQ").then(info => {
+            console.log(info.formats);
+        });
+    }
+}
+
