502ad47dbd02a1ad27e4697182ca67c5f35173d0
Gabriel Barbosa <gabrielbarbosaprofissional@gmail.com>
2025-08-04 17:59:46 -0300

    Wip: Initial logic example

--- src/index.js
+++ src/index.js
@@ -1,18 +1,88 @@
-import { createServer } from 'http'
-import { statusCodes } from './utils/httpStatusCode.js'
-import DownloadService from './service/download.service.js'
+// import { createServer } from 'http'
+// import { statusCodes } from './utils/httpStatusCode.js'
+// import DownloadService from './service/download.service.js'
 
 
 
 
-async function handler(request, response) {
-    const downloadService = new DownloadService()
+// async function handler(request, response) {
+//     const downloadService = new DownloadService()
 
-    // Example usage of the download service
-    await downloadService.downloadFile();
+//     // Example usage of the download service
+//     await downloadService.downloadFile();
 
-    response.writeHead(statusCodes.OK)
-    response.end()
+//     response.writeHead(statusCodes.OK)
+//     response.end()
+// }
+
+// createServer(handler).listen(3000, () => console.log('running at 3000'))
+
+
+// import youtubedl from 'youtube-dl-exec';
+
+// async function downloadVideo(url, output = 'video.mp4') {
+//     try {
+//         await youtubedl(url, {
+//             output,
+//             format: 'bestvideo+bestaudio/best',
+//             mergeOutputFormat: 'mp4',
+//             // verbose: true, // para mais logs
+//             // noCheckCertificate: true,
+//             // noWarnings: true,
+//         });
+//         console.log('Download finalizado:', output);
+//     } catch (err) {
+//         console.error('Erro no download:', err);
+//     }
+// }
+
+// const url = 'https://www.youtube.com/watch?v=OU8PQiIKKXo&ab_channel=HolderDawn';
+// downloadVideo(url);
+
+
+
+import youtubedl from 'youtube-dl-exec';
+import ffmpegPath from 'ffmpeg-static';
+import { exec } from 'child_process';
+import fs from 'fs/promises';
+
+async function baixarEConverter(url) {
+    const videoFile = 'audio.webm';
+    const mp3File = 'audio.mp3';
+
+    try {
+        // Baixa áudio
+        await youtubedl(url, {
+            output: videoFile,
+            format: 'bestaudio/best',
+        });
+        console.log('✅ Áudio baixado:', videoFile);
+
+        // Converte para MP3 com bitrate 320 kbps e frequência 48kHz
+        await new Promise((resolve, reject) => {
+            exec(`${ffmpegPath} -i ${videoFile} -vn -ab 320k -ar 48000 -y ${mp3File}`, (error, stdout, stderr) => {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+                console.log('✅ Conversão para mp3 (qualidade máxima) concluída:', mp3File);
+                resolve();
+            });
+        });
+
+        // Remove arquivo temporário
+        await fs.unlink(videoFile);
+        console.log('🗑️ Arquivo temporário removido:', videoFile);
+
+    } catch (err) {
+        console.error('❌ Erro:', err);
+    }
 }
 
-createServer(handler).listen(3000, () => console.log('running at 3000'))
+
+
+const url = 'https://www.youtube.com/watch?v=OU8PQiIKKXo&ab_channel=HolderDawn';
+baixarEConverter(url);
+
+
+
--- src/service/download.service.js
+++ src/service/download.service.js
@@ -1,14 +1,15 @@
-import * as ytdl from '@distube/ytdl-core';
+import fs from 'fs';
+import * as ytdl from '@ytdl-core';
 
 // Polyfill para File no Node.js (corrige erro do undici)
 if (typeof File === 'undefined') {
-  global.File = class File extends Blob {
-    constructor(chunks, filename, options = {}) {
-      super(chunks, options);
-      this.name = filename;
-      this.lastModified = options.lastModified || Date.now();
-    }
-  };
+    global.File = class File extends Blob {
+        constructor(chunks, filename, options = {}) {
+            super(chunks, options);
+            this.name = filename;
+            this.lastModified = options.lastModified || Date.now();
+        }
+    };
 }
 
 export default class DownloadService {
@@ -21,17 +22,8 @@ export default class DownloadService {
         // TypeScript: import ytdl = require('@distube/ytdl-core'); with neither of the above
 
         // Download a video
-        ytdl("http://www.youtube.com/watch?v=aqz-KE-bpKQ").pipe(require("fs").createWriteStream("video.mp4"));
-
-        // Get video info
-        ytdl.getBasicInfo("http://www.youtube.com/watch?v=aqz-KE-bpKQ").then(info => {
-            console.log(info.videoDetails.title);
-        });
-
-        // Get video info with download formats
-        ytdl.getInfo("http://www.youtube.com/watch?v=aqz-KE-bpKQ").then(info => {
-            console.log(info.formats);
-        });
+        ytdl('http://www.youtube.com/watch?v=aqz-KE-bpKQ')
+            .pipe(fs.createWriteStream('video.mp4'));
     }
 }
 
